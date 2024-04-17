1f90d5f04453d940bdf234f09538136c2e133ebf
Safar123 <azajsbd111@gmail.com>
2024-04-17 10:50:40 +1000

    basic CRUD fo0r User Model

---
+++ Controller/userController.js
@@ -0,0 +1,21 @@
+const User = require('../Model/userModel');
+
+exports.getUser= (req, res)=>{
+
+}
+
+exports.getSingleUser= (req,res)=>{
+    
+}
+
+exports.createUser= (req,res)=>{
+
+}
+
+exports.updateUser = (req,res)=>{
+
+}
+
+exports.deleteUser = (req,res)=>{
+
+}
