9c955a699858fb44ad6145164237986600f01113
Safar123 <azajsbd111@gmail.com>
2024-04-18 16:20:05 +1000

    sign up and get all user

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,6 +1,24 @@
 const User = require('../Model/userModel');
 
-exports.getUser= (req, res)=>{
+exports.getUser= async (req, res)=>{
+try{
+    const allUser = await User.find();
+    if (!allUser){
+        res.status(200).json({
+            status:'success',
+            message:'No user yet'
+        })
+    } 
+
+    res.status(200).json({
+        status:'success',
+        users: allUser
+    })
+ 
+}
+catch(err){
+        
+}
 
 }
 
@@ -8,7 +26,23 @@ exports.getSingleUser= (req,res)=>{
     
 }
 
-exports.createUser= (req,res)=>{
+exports.createUser= async (req,res)=>{
+    try{
+        const newUser= await User.create(req.body);
+        res.status(201).json({
+            status:'success',
+            data:{
+                user:newUser
+            }
+        })
+    }
+   catch(err){
+    res.status(400).json({
+        status:'fail',
+        message:err
+    })
+
+   }
 
 }
 
