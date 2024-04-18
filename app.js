dae35ed54a8083e8a61765caac139ee71f32f56d
Safar123 <azajsbd111@gmail.com>
2024-04-18 16:20:27 +1000

    implementing userRouter

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require ('express');
 const morgan = require('morgan');
 
 const app = express();
+app.use(express.json());
+
+const userRoute = require('./Routes/userRoutes');
 
 if (process.env.NODE_ENV ==='development'){
     app.use(morgan('dev'));
 }
-app.get('/', (req,res)=>{
-    res.status(200).json({message:'Hello from the server side',
-app:'Recipe Manager'});
-})
+
+app.use('/api/v1', userRoute);
 
 module.exports=app;
