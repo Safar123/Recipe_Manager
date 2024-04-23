6d068cb57dae9ead80471519adda7be84c6583e2
Safar123 <azajsbd111@gmail.com>
2024-04-23 10:28:02 +1000

    Implementing updated recipe routes

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express();
 app.use(express.json());
 
 const userRoute = require('./Routes/userRoutes');
-
+const recipeRoute = require('./Routes/recipeRoute');
 
 if (process.env.NODE_ENV ==='development'){
     app.use(morgan('dev'));
 }
 
-app.use('/api/v1', userRoute);
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/recipe', recipeRoute);
 
 module.exports=app;
