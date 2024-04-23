a92070f658b74c0daacbef1b13f781bb52357a2c
Safar123 <azajsbd111@gmail.com>
2024-04-23 10:59:41 +1000

    Filtering

--- Controller/recipeController.js
+++ Controller/recipeController.js
@@ -3,7 +3,14 @@ const Recipe = require('../Model/recipeModel');
 exports.getAllRecipe = async (req, res)=>{
 
     try{
-        const allRecipe = await Recipe.find();
+        const queryObj = {...req.query};
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match=>`$${match}`);
+
+        const query = Recipe.find(JSON.parse(queryStr));
+        const allRecipe = await query;
         if(!allRecipe || allRecipe.length === 0){
 
     res.status(404).json({
