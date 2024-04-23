49c559d4bf867d61287eebcd8aee3d3ae819b9b6
Safar123 <azajsbd111@gmail.com>
2024-04-23 17:17:42 +1000

    implementing aliass routes

--- Routes/recipeRoute.js
+++ Routes/recipeRoute.js
@@ -3,6 +3,8 @@ const express = require('express');
 const recipeController= require('../Controller/recipeController');
 const router = express.Router();
 
+router.route('/top-5-recipe').get(recipeController.top5recipe, recipeController.getAllRecipe);
+
 router.route('/')
 .get(recipeController.getAllRecipe)
 .post(recipeController.generateRecipe);
