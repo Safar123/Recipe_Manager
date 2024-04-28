ca3f62f8ae6859672527310a1d270ba3034b477f
Safar123 <azajsbd111@gmail.com>
2024-04-28 21:15:59 +1000

    N updated routes for auth control and general request for user

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,15 +1,43 @@
-const express = require('express');
+const express = require("express");
+const {
+    signUpUser,
+    logInUser,
+    resizeUserImage,
+    uploadUserImage,
+    protectRoute,
+    authorizationRoutes,
+    forgetPassword,
+    resetPassword,
+    updatePassword,
+    updateMe,
+} = require("../Controller/authController");
+const {
+    getAllUser,
+    getSingleUser,
+    updateUserSelf,
+    deleteUser,
+} = require("../Controller/userController");
 
-const userController = require('../Controller/userController');
 const router = express.Router();
 
-router.route('/')
-.get(userController.getUser)
-.post(userController.createUser);
+router.route("/signUp").post(uploadUserImage, resizeUserImage, signUpUser);
 
-router.route('/:id')
-.patch(userController.updateUser)
-.delete(userController.deleteUser)
-.get(userController.getSingleUser);
+router .route("/")
+    .get(protectRoute, authorizationRoutes("admin", "superadmin"), getAllUser);
+
+router .route("/:id")
+    .get(protectRoute, getSingleUser)
+    .patch(protectRoute, updateUserSelf)
+    .delete(protectRoute, deleteUser);
+
+router.route("/login").post(logInUser);
+
+router.patch("/resetPassword/:token", resetPassword);
+
+router.post("/forgetPassword", forgetPassword);
+
+router.patch("/login/updatePassword", protectRoute, updatePassword);
+
+router.patch("/updateMe", protectRoute, updateMe);
 
 module.exports = router;
