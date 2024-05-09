4c5a291637da08cd2162028509f786063fe063de
Safar123 <azajsbd111@gmail.com>
2024-05-09 11:42:02 +1000

    adding auth controller and nested routes

--- Routes/recipeRoute.js
+++ Routes/recipeRoute.js
@@ -1,20 +1,31 @@
 const express = require('express');
 
 const recipeController= require('../Controller/recipeController');
+const reviewRouter = require('../Routes/reviewRoutes');
+const authController = require('../Controller/authController');
 const router = express.Router();
 
 router.route('/top-5-recipe').get(recipeController.top5recipe, recipeController.getAllRecipe);
 
+router.use('/:recipeId/reviews', reviewRouter);
+
 router.route('/')
-.get(recipeController.getAllRecipe)
-.post(recipeController.uploadRecipeImage, recipeController.resizeRecipeImage, recipeController.generateRecipe);
+.get(authController.protectRoute, recipeController.getAllRecipe)
+.post(authController.protectRoute, 
+    recipeController.uploadRecipeImage, 
+    recipeController.resizeRecipeImage, 
+    recipeController.generateRecipe);
 
 router.route('/:id')
-.patch(recipeController.uploadRecipeImage, recipeController.resizeRecipeImage, recipeController.updateRecipe)
+.patch(recipeController.uploadRecipeImage, 
+    recipeController.resizeRecipeImage, 
+    recipeController.updateRecipe)
 .delete(recipeController.removeRecipe)
 .get(recipeController.getSingleRecipe);
 
 router.route('/:recipeId/favorite').patch(recipeController.markAsFavorite);
 router.route('/favorites/:userId').get(recipeController.getUserFavorites);
+
+
 
 module.exports = router;
