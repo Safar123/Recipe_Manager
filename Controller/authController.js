d470dee7bdac5b2b148a7f636a576999717ae6f4
Safar123 <azajsbd111@gmail.com>
2024-05-09 11:37:05 +1000

    minimal changes

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,11 +1,13 @@
 const crypto = require('crypto')
 const User = require("../Model/userModel");
 const catchAsync = require("../utils/catchAsyncError");
-const GlobalError = require("../utils/globalError");
+const AppError = require("../utils/globalError");
 const sharp = require("sharp");
 const multer = require("multer");
 const {generateToken, verifyToken} = require('../utils/jwtTokenHandler');
 const sendResetEmail = require('../utils/passwordResetMail');
+
+
 
 const filterObj =(obj, ...fields)=>{
 const newObj = {};
@@ -25,7 +27,7 @@ const multerFilter = (req, file, cb) => {
         cb(null, true);
     } else {
         cb(
-            new GlobalError("Invalid file type. Please upload image only", 400),
+            new AppError("Invalid file type. Please upload image only", 400),
             false
         );
     }
@@ -53,27 +55,24 @@ exports.resizeUserImage =catchAsync( async (req, res, next) => {
 });
 
 exports.signUpUser = catchAsync(async (req, res, next) => {
-    try {
-        console.log("signUpUser", req.body);
-        
+       
         if (!req.body.password.match(/^(?=.*[a-zA-Z])(?=.*[0-9]).*$/)) {
             return next(
-                new GlobalError("Password must contain both number and letter", 401)
+                new AppError("Password must contain both number and letter", 401)
             );
         }
         const newUser = await User.create({
             email: req.body.email,
             password: req.body.password,
-            name: req.body.fullname,
-            role: 'admin',
-            username: req.body.username
+            confirmPassword:req.body.confirmPassword,
+          
         });
+        generateToken(newUser, 201, res);
 
-        generateToken(newUser, 201, res)
-    } catch (error) {
-        console.log(error);
-        return next(new GlobalError(`Error creating user: ${error.message}`, 500));
-    }
+        if(!newUser){
+            return next (new AppError(`Something went wrong while signing up. Please check user credintials`, 400) )
+        }
+    
 });
 
 //!Login function
@@ -84,7 +83,7 @@ exports.logInUser = catchAsync(async (req, res, next) => {
 
     //!check we have both emil and password
     if (!email || !password) {
-        return next(new GlobalError("Email and Password is required field", 401));
+        return next(new AppError("Email and Password is required field", 401));
     }
     //!then we need to check if email is registered or not
     const user = await User.findOne({ email }).select("+password");
@@ -92,7 +91,7 @@ exports.logInUser = catchAsync(async (req, res, next) => {
     //!function to compre user input pwd with encrypted password in database
     if (!user || !(await user.checkPwdEncryption(password, user.password))) {
         return next(
-            new GlobalError(
+            new AppError(
                 "Either email or password didnt match user credentials",
                 401
             )
@@ -162,7 +161,7 @@ exports.authorizationRoutes = (...roles) => {
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
 
-            return next(new GlobalError(' You do not have access to complete this action', 403));
+            return next(new AppError(' You do not have access to complete this action', 403));
         }
 
         next();
@@ -172,12 +171,12 @@ exports.authorizationRoutes = (...roles) => {
 exports.forgetPassword = catchAsync(async(req,res,next)=>{
 
     if(!req.body.email){
-        return next(new GlobalError('Please provide email address', 400 ))
+        return next(new AppError('Please provide email address', 400 ))
     }
     const user = await User.findOne({email:req.body.email});
 
     if(!user){
-        return next (new GlobalError('No user is registered with provided email', 404))
+        return next (new AppError('No user is registered with provided email', 404))
     }
 
     const resetToken = user.generatePasswordResetToken();
@@ -185,7 +184,7 @@ exports.forgetPassword = catchAsync(async(req,res,next)=>{
 
     //!defining url to password reset and sending email
 
-    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/user/resetPassword/${resetToken}`;
     const message = `Forgot your password? If yes then use this link to reset your password using PATCH request on ${resetURL} URL
     \n If this is not what you have requested please kindly ignore this email. \n Thank You `;
     
@@ -207,7 +206,7 @@ try{
     user.passwordTokenExpire= undefined;
     await user.save({validateBeforeSave:false});
 
-    return next(new GlobalError('Something went wrong while sending email to user'), 500);
+    return next(new AppError('Something went wrong while sending email to user'), 500);
 }
 
 })
@@ -215,7 +214,7 @@ try{
 exports.resetPassword = catchAsync(async(req,res,next)=>{
 
     if(req.params.token.length<64){
-        return next (new GlobalError('Please provide valid token'), 400)
+        return next (new AppError('Please provide valid token'), 400)
     }
 
     const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
@@ -226,12 +225,12 @@ exports.resetPassword = catchAsync(async(req,res,next)=>{
          }}).select('+password')
 
          if(!user){
-            return next(new GlobalError('Invalid reset token or token has expired'), 400)
+            return next(new AppError('Invalid reset token or token has expired'), 400)
          }
     
      const newPassHash = await user.checkPwdEncryption(req.body.password, user.password)
 if (newPassHash){
-    return next(new GlobalError('Thats your current password. Either login or provide new password'))
+    return next(new AppError('Thats your current password. Either login or provide new password'))
 }
          user.password = req.body.password;
          user.confirmPassword = req.body.confirmPassword;
@@ -248,19 +247,19 @@ if (newPassHash){
 exports.updatePassword = catchAsync(async(req,res,next)=>{
     const user = await User.findById(req.user.id).select('+password');
     if(!user){
-        return next(new GlobalError('This is protected route, so must log in.'),403)
+        return next(new AppError('This is protected route, so must log in.'),403)
     }
    if(!req.body.currentPassword){
-    return next(new GlobalError('Please provide your current password'), 400)
+    return next(new AppError('Please provide your current password'), 400)
    }
    const comparePassword = await user.checkPwdEncryption(req.body.currentPassword, user.password)
 
    if(!comparePassword){
-    return next(new GlobalError('Password wrong, please provide correct password'),400)
+    return next(new AppError('Password wrong, please provide correct password'),400)
    }
 
    if(!req.body.password || !req.body.confirmPassword){
-    return next (new GlobalError('Password and password confirm field is required'),400)
+    return next (new AppError('Password and password confirm field is required'),400)
    }
 
    user.password=req.body.password;
@@ -273,7 +272,7 @@ exports.updatePassword = catchAsync(async(req,res,next)=>{
 exports.updateMe = catchAsync(async (req,res,next)=>{
 
     if(req.body.password|| req.body.confirmPassword){
-        return next(new GlobalError('To update your password follow "/updatePassword" '))
+        return next(new AppError('To update your password follow "/updatePassword" '))
     }
 
     const filteredBody = filterObj(req.body, 'email'  ) 
@@ -282,7 +281,7 @@ exports.updateMe = catchAsync(async (req,res,next)=>{
         runValidators:true
     });
     if(!user){
-        return next (new GlobalError('No user found for email'),404)
+        return next (new AppError('No user found for email'),404)
     }
 
 })
