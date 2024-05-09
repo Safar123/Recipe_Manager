8815abae671741c4b20c38fe68e4875ed422e1cc
Safar123 <azajsbd111@gmail.com>
2024-05-09 11:43:16 +1000

    adding user ddetails from logged in user information

--- Controller/reviewController.js
+++ Controller/reviewController.js
@@ -5,17 +5,26 @@ const AppError = require('../utils/globalError');
 
 exports.createReview = catchAsync(async (req,res,next)=>{
 
+    if(!req.body.recipe) req.body.recipe = req.params.recipeId;
+    if(!req.body.user) req.body.user = req.user.id;
+
     const newReview = await Review.create(req.body);
     res.status(201).json({
         status:'success',
         review:newReview
     })
+    if(!newReview){
+        return next (new AppError(' Something went wrong while posting your review', 400))
+    }
 });
 
 
 exports.getReview = catchAsync(async(req,res, next)=>{
 
-    const reviews = await Review.find();
+    let filter = {}
+    if(req.params.recipeId) filter = {recipe: req.params.recipeId}
+
+    const reviews = await Review.find(filter);
 
     res.status(200).json({
         status:'success',
